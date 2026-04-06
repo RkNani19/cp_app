8ce1f281183ccb3bc66fc5f1b18dc76718c943ee
ravindrakesuboina <ravindrrakumar11539@gmail.com>
2026-04-06 15:27:10 +0530

    Update login screen and add create account

---
+++ lib/view/create_account.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CreateAccount extends StatefulWidget {
+  const CreateAccount({super.key, required this.title});
+  final String title;
+
+  @override
+  State<CreateAccount> createState() => _CreateAccountState();
+}
+
+class _CreateAccountState extends State<CreateAccount> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gjk_cp/view/create_account.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key, required this.title});
@@ -44,68 +45,99 @@ class _LoginScreenState extends State<LoginScreen> {
                       "Customer Portal Login",
                       style: TextStyle(fontSize: 16, color: Colors.grey),
                     ),
-
-                    SizedBox(height: 20),
                   ],
                 ),
               ),
             ),
             SizedBox(height: 10),
-            Expanded(
+            Center(
               child: Container(
-                margin: const EdgeInsets.symmetric(horizontal: 14),
-                padding: EdgeInsets.all(20),
+                  width: MediaQuery.of(context).size.width * 0.9,
+               margin: const EdgeInsets.symmetric(vertical: 10),
+                padding: const EdgeInsets.all(16),
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(15),
-                  boxShadow: [BoxShadow(blurRadius: 10)],
+                  boxShadow: [BoxShadow(blurRadius: 6)],
                 ),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Row(
-                      children: [ Text(
+                    Text(
                       "Welcome Back",
                       style: TextStyle(
                         fontSize: 18,
                         fontWeight: FontWeight.bold,
                       ),
-
                     ),
-                      ]
-                    ),
-                     const SizedBox(height: 10),
+                    const SizedBox(height: 10),
 
-                     const Text(
+                    const Text(
                       "Enter your credentials to access your account",
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.grey,
+                      style: TextStyle(fontSize: 16, color: Colors.grey),
+                    ),
+
+                    SizedBox(height: 10),
+                    Text(
+                      "Customer ID/Mobile Number",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: "Enter Customer ID or Mobile",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        isDense: true,
                       ),
                     ),
 
-                    SizedBox(height: 10,),
-                    Text("Customer ID/Mobile Number"),
-                    TextField(decoration: InputDecoration(
-                      labelText: "Enter Customer ID or Mobile",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
+                    SizedBox(height: 10),
+                    Text("Secret Key", style: TextStyle(fontSize: 16)),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: "Enter your secret key",
+                        prefixIcon: Icon(Icons.key_outlined),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        isDense: true,
                       ),
-                      isDense: true,
                     ),
-                    ),
-
-                    SizedBox(height: 10,),
-                    Text("Secret Key"),
-                    TextField(decoration: InputDecoration(
-                      labelText: "Enter your secret key",
-                      prefixIcon:Icon(Icons.key_outlined),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10)
-                      ) 
-                    ),)
                   ],
                 ),
               ),
+            ),
+
+            const SizedBox(height: 8),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Don't have an account? ",
+                  style: TextStyle(fontSize: 14, color: Colors.grey),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            const CreateAccount(title: 'Create Account'),
+                      ),
+                    );
+                  },
+                  child: const Text(
+                    "Create Account",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Color.fromARGB(255, 198, 218, 132),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
